3a6e0c5fae82834a4f044defaac0f476b8e7701b
Erick Pacheco <linarespachecoerick@gmail.com>
2022-08-06 20:09:25 -0400

    Refactor server to handle error route when MONGO_URL is not present.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const init = require("./server");
 const app = require("./app");
 
 const config = {
-  PORT: process.env.PORT || 3001,
-  MONGO_URL: process.env.MONGO_URL,
-  callback: () => console.log(`App running`),
+	PORT: process.env.PORT || 3001,
+	MONGO_URL: process.env.MONGO_URL,
 };
 
 init(app, config);
