b8e2d2593f0258ec62e64734cac27f8e11e9aa6d
OpenTechWebsite <opentechwebsite@gmail.com>
2022-08-22 21:08:30 -0400

    Remove authenticationi for develoment purposes

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -1,8 +1,8 @@
 const app = require("./app.middleware");
 const routes = require("../routes");
-const { checkJwt } = require("../auth");
+// const { checkJwt } = require("../auth");
 
 app.use("/", routes.app);
-app.use("/api/tasks", checkJwt, routes.tasks);
+app.use("/api/tasks", routes.tasks);
 
 module.exports = app;
